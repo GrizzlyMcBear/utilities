44fe3d5ec2eb90fb67b4116c3a8863ffc07a1a1a
GrizzlyMcBear <sronenco@gmail.com>
2022-10-01 23:23:46 +0300

    Added a new solution and a project to contain the source code (provided by MOE) for the classes: * Node * Stack

---
+++ VS/utils/ADT/Node.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADT {
+
+	/// <summary>
+	/// This class is the implementation for the Node, as written by the MOE (Ministry Of Education)
+	/// </summary>
+	public class Node<T> {
+
+		/// <summary>
+		/// The value of the current node.
+		/// </summary>
+		private T value;
+
+		/// <summary>
+		/// The reference to the next node.
+		/// </summary>
+		private Node<T> next;
+
+		/// <summary>
+		/// C'tor which creates a node with a `value` field and no next node.
+		/// </summary>
+		/// <param name="value">
+		/// The value (of the specified type T) the node would be initialized with.
+		/// </param>
+		public Node(T value) {
+			this.value = value;
+			this.next = null;
+		}
+
+		/// <summary>
+		/// C'tor which creates a node with both a `value` field and a next node.
+		/// </summary>
+		/// <param name="value">
+		/// The value (of the specified type {@link T}) the node would be initialized with.
+		/// </param>
+		/// <param name="next">
+		/// The node following this one (this node is linked to the next node).
+		/// </param>
+		public Node(T value, Node<T> next) {
+			this.value = value;
+			this.next = next;
+		}
+
+		/// <summary>
+		/// This method returns the value of the current node.
+		/// </summary>
+		/// <returns>
+		/// The value of the current node.
+		/// </returns>
+		public T GetValue() {
+			return value;
+		}
+
+		/// <summary>
+		/// This method returns the node which follows the current node.
+		/// </summary>
+		/// <returns>
+		/// The reference to the next node.
+		/// </returns>
+		public Node<T> GetNext() {
+			return next;
+		}
+
+		/**
+		 * This method sets (updates) the value stored in the current node to `value`.
+		 * @param value
+		 */
+		public void SetValue(T value) {
+			this.value = value;
+		}
+
+		/**
+		 * This method sets the node which follows the current node to be `next`.
+		 * @param next
+		 */
+		public void SetNext(Node<T> next) {
+			this.next = next;
+		}
+
+		/// <summary>
+		/// This method checks if the current node has a next node.
+		/// </summary>
+		/// <returns>
+		/// `true` if a next node exists and `false` otherwise.
+		/// </returns>
+		public bool HasNext() {
+			return this.next != null;
+		}
+
+		/**
+		 * This method returns a {@link String} which describes the current node (and any nodes which follow it).
+		 */
+		override public String ToString() {
+			//return this.value + "  " + next;
+			return String.Format("-> [ {0} ] {1}",
+				GetValue(), GetNext() == null ? "-|" : GetNext().ToString());
+		}
+	}
+}
---
+++ VS/utils/ADT/Stack.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADT {
+	public class Stack<T> {
+		
+		// תכונת המחלקה
+		private Node<T> first;
+
+		// פעולה בונה היוצרת ומחזירה מחסנית ריקה
+		public Stack() {
+			this.first = null;
+		}
+
+		// פעולה המחזירה `אמת` אם המחסנית ריקה ו-`שקר` אחרת
+		public bool IsEmpty() {
+			return this.first == null;
+		}
+
+		// דחיפה למחסנית - פעולה הדוחפת איבר לראש המחסנית
+		public void Push(T x) {
+			this.first = new Node<T>(x, this.first);
+		}
+
+		// שלוף ממחסנית - פעולה המוציאה ומחזירה את האיבר שבראש המחסנית
+		// הנחה: המחסנית אינה ריקה
+		public T Pop() {
+			T x = this.first.GetValue();
+			this.first = this.first.GetNext();
+			return x;
+		}
+
+		// הצץ למחסנית - פעולה המחזירה את האיבר שבראש המחסנית מבלי להוציאו
+		// הנחה: המחסנית לא ריקה
+		public T Top() {
+			return this.first.GetValue();
+		}
+
+		// פעולה המחזירה מחרוזת המתארת את מצב המחסנית באופן הבא
+		// [x1, x2, ..., xn]
+		// x1 כאשר בראש המחסנית נמצא האיבר
+		// xn ואילו בסוף המחסנית נמצא האיבר
+		public override string ToString() {
+			string str = "[";
+			Node <T> pos = this.first;
+			
+			while (pos != null) {
+				str += pos.GetValue().ToString();
+				if (pos.HasNext())
+					str += ", ";
+
+				pos = pos.GetNext();
+			}
+
+			str += "]";
+
+			return str;
+		}
+	}
+}
