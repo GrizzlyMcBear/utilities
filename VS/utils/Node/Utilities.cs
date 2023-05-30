84b28595363ecb84ac082e4a92c75e6d77520013
GrizzlyMcBear <sronenco@gmail.com>
2023-05-30 23:53:27 +0300

    Added a class with utility methods for a Node.

--- VS/utils/Node/Utilities.cs
+++ VS/utils/Node/Utilities.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MOE.Node {
 	public class Utilities {
-		public static Node<int> CreateList(string rawNumbers) {
+		public static Node<int> CreateLinkedList(string rawNumbers, char separator) {
 			
 			// Edge cases for the empty list
 			if (rawNumbers == null || rawNumbers.Length == 0) return null;
@@ -29,6 +29,18 @@ namespace MOE.Node {
 
 			lastNode.SetNext(null);
 			return list;
+		}
+
+		// todo: add input validation
+		public static List<Node<int>> CreateLinkedLists(string rawNumbers, char innerSep, char outerSep) {
+			List<Node<int>> lists = new List<Node<int>>();
+			string[] linkedListsRawValues = rawNumbers.Split(outerSep);
+
+			foreach (string listRawValues in linkedListsRawValues) {
+				lists.Add(CreateLinkedList(listRawValues, innerSep));
+			}
+
+			return lists;
 		}
 
 		/// <summary>
@@ -71,6 +83,19 @@ namespace MOE.Node {
 			}
 
 			return amount;
+		}
+	}
+	public class Utilities<T> {
+		public static int Length(Node<T> list) {
+			if (list == null) return 0;
+
+			int length = 0;
+			while (list != null) {
+				length++;
+				list = list.GetNext();
+			}
+
+			return length;
 		}
 	}
 }
